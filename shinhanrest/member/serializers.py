90e1a3dbafc69d9f4bccfeae7b010852d8adf582
MinjeMun <mmj010220@gmail.com>
2023-01-26 10:05:30 +0900

    feat: Create signup page

--- shinhanrest/member/serializers.py
+++ shinhanrest/member/serializers.py
@@ -10,9 +10,24 @@ class MemberSerializer(serializers.ModelSerializer):
         # 유효성 검사가 끝난 값을 반환
         return make_password(value)
 
+
+
+    # 비밀번호 일치 여부를 백엔드에서 처리할 경우
+
+    # password_check = serializers.CharField()
+
+    # def validate(self, attrs):
+    #     if len(attrs['password']) < 8:
+    #         raise serializers.ValidationError('Too short password')
+
+    #     if attrs['password'] != attrs['password_check']:
+    #         raise serializers.ValidationError('비밀번호 일치하지 않음')
+
+    #     attrs['password'] = make_password(attrs['password'])
+
     class Meta:
         model = Member
-        fields = ('id','username','tel','password')
+        fields = ('id','username','tel','password',)
         extra_kwargs = {
             'id': {
                 'read_only': True,
